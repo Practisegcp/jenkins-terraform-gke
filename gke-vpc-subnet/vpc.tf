b183902241092eca4703d927e377f183bd62e0f0
root <root@jenkins-kra-vm.us-east1-b.c.skillful-octane-393107.internal>
2023-07-24 18:25:21 +0000

    tf-pip-gke

--- gke-vpc-subnet/vpc.tf
+++ gke-vpc-subnet/vpc.tf
@@ -11,13 +11,13 @@ provider "google" {
 
 # VPC
 resource "google_compute_network" "vpc" {
-  name                    = "${var.project_id}-vpc"
+  name                    = "kr-vpc"
   auto_create_subnetworks = "false"
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
+  name          = "kr-subnet"
   region        = "us-central1"
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
