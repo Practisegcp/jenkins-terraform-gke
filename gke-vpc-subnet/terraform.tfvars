feb960fa955e796903ae1a304fca722fd7ff42b9
root <root@jenkins-kra-vm.us-east1-b.c.skillful-octane-393107.internal>
2023-07-24 18:06:34 +0000

    private gke cluster

---
+++ gke-vpc-subnet/terraform.tfvars
@@ -0,0 +1,1 @@
+project_id = "skillful-octane-393107"
---
+++ gke-vpc-subnet/vpc.tf
@@ -0,0 +1,24 @@
+variable "project_id" {
+  description = "project id"
+}
+
+
+provider "google" {
+  project     = var.project_id
+  region      = "us-central1"
+  credentials = "/var/lib/jenkins/workspace/jenkins-terraform-gke/gke-vpc-subnet/sa.json"
+}
+
+# VPC
+resource "google_compute_network" "vpc" {
+  name                    = "${var.project_id}-vpc"
+  auto_create_subnetworks = "false"
+}
+
+# Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.project_id}-subnet"
+  region        = "us-central1"
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.10.0.0/24"
+}
